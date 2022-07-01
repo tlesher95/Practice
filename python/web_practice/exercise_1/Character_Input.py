a659cdd4260266e954ed529f5e5d178a899a31cb
gmlesher <70789983+gmlesher@users.noreply.github.com>
2022-07-01 00:32:00 -0500

    suggested exercise 1 improvements

--- python/web_practice/exercise_1/Character_Input.py
+++ python/web_practice/exercise_1/Character_Input.py
@@ -1,4 +1,4 @@
-nam = input("Enter Name Here:")
-age = int(float(input("Enter Current Age Here:")))
+name = input("Enter Name Here: ")
+age = round(float(input("Enter Current Age Here: ")))
 century = (2022 - age) + 100
-print(2 * (nam, ", you will be 100 years old in the year", century))
+print(2 * (name, ", you will be 100 years old in the year", century))
